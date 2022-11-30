f433a1ac08ac83785986fedaef5d568915164bea
Damian Olszewski <damianolszewski25@gmail.com>
2022-11-30 17:07:31 +0100

    Update TvShows.jsx

--- src/components/TvShows.jsx
+++ src/components/TvShows.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SingleCarousel from "./SingleCarousel";
-const TvShows = (props) => {
+const TvShows = () => {
   <Container fluid>
     <SingleCarousel search="Game of Thrones" />
   </Container>;
